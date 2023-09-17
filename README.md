9e9976992485529e051e1c2e907695b66e1338f8
silent1603 <tamgiangbui@gmail.com>
2023-09-17 23:48:21 +0700

    add script to build

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# LTSS
+# Ray Tracing
+
+## Prerequisite
+
+- [CMake](https://cmake.org/download/)
+
+- GCC / CLANG complier
+
+## How to build project using script
+
+- Window: Build.bat
+
+- Mac/Linux: Build.sh
+
+## How to run project using script
+
+- Window: Run.bat
+
+- Mac/Linux: Run.sh
+
+## How to manual build project and run
+
+- mkdir build
+- cd build
+- cmake ..
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <thread>
 #include "openmp/omp.h"
+#include "color.h"
+#include "math.h"
 
-int main()
-{
-	return 0;
+int main() {
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
 }
